85dbf2eaa773b9964b0b990b46866f10a1db132a
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-11-13 11:25:31 +0100

    remove `check_result`

--- xraylib_numba/config.py
+++ xraylib_numba/config.py
@@ -1,10 +1,6 @@
 """_summary_
 """
 
-check_result = True
-"""
-Whether to check if the result of a function is 0.0 and raise a warning if it is.
-"""
 
 allow_Nd = False
 # Whether to allow N-dimensional arrays as input to xraylib_np functions.
