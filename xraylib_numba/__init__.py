75809379b370ee1e8b4bd127ffe9704d019d7262
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2024-05-13 14:47:23 +0200

    entry points

--- xraylib_numba/__init__.py
+++ xraylib_numba/__init__.py
@@ -1,6 +1,15 @@
-"""_summary_
-"""
+"""_summary_."""
 
 __version__ = "0.1.0"
 
-from . import xraylib_overloads
+# from . import xraylib_overloads
+from .config import config
+
+
+def _init() -> None:
+    from . import xraylib_overloads
+
+
+# __all__ = ["xraylib_overloads"]
+
+__all__ = ["config", "xraylib_overloads"]
